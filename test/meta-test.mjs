cd42709f4c61eec11d0295fb4f2e5c707c43d0dc
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2014-01-13 15:10:10 +0100

    Use empty tuple as a shortcut to undefined.

--- test/meta-test.mjs
+++ test/meta-test.mjs
@@ -24,160 +24,184 @@ meta
       code.replaceTag('body', expr.argAt(1))
       give code
 
-describe 'Metascript' do
+describe 'Metascript' do (
 
-it 'Should work' do
+it 'Should evaluate simple expression' do
+  (if true 1 else 2).should.equal 1
 
-
-(if true 1 else 2).should.equal 1
-
-var test = () -> do
+it 'Should handle tuple assignments' do
   var (a, b) = if true (1, 2) else (2, 1)
   a.should.equal(1)
   b.should.equal(2)
   (a, b) = (b, a)
   a.should.equal(2)
   b.should.equal(1)
-test()
 
-var f = (x) ->
-  loop (var r = 1, x)
-    if (x > 0)
-      next (r * x, x - 1)
-    else
-      give r
+it 'Should handle loops' do
+  var f = (x) ->
+    loop (var r = 1, x)
+      if (x > 0)
+        next (r * x, x - 1)
+      else
+        give r
+  f(1).should.equal(1)
+  f(2).should.equal(2)
+  f(3).should.equal(6)
+  f(6).should.equal(6 * 5 * 4 * 3 * 2)
 
-f(1).should.equal(1)
-f(2).should.equal(2)
-f(3).should.equal(6)
-f(6).should.equal(6 * 5 * 4 * 3 * 2)
 
-var g = (x, y) -> do
-  var (a, b) = do
-    if (x > 0)
-      give (1, 2)
-    if (y > 0)
-      give (3, 6)
-    if (x + y < 0)
-      give (4, 8)
-    give (5, 10)
-  return (b - a)
+it 'Should handle nested do and if' do
+  var g = (x, y) -> do
+    var (a, b) = do
+      if (x > 0)
+        give (1, 2)
+      if (y > 0)
+        give (3, 6)
+      if (x + y < 0)
+        give (4, 8)
+      give (5, 10)
+    return (b - a)
+  g(1, 2).should.equal(1)
+  g(-1, 2).should.equal(3)
+  g(-1, -2).should.equal(4)
+  g(NaN, -2).should.equal(5)
 
-g(1, 2).should.equal(1)
-g(-1, 2).should.equal(3)
-g(-1, -2).should.equal(4)
-g(NaN, -2).should.equal(5)
 
-var obj = {
-  a: 1
-  b: 2
-}
-obj.should.have.property('a', 1);
-obj.should.have.property('b', 2);
+it 'Should handle object literals' do
+  var obj = {
+    a: 1
+    b: 2
+  }
+  obj.should.have.property('a', 1)
+  obj.should.have.property('b', 2)
 
-var arr = [ 1, 2, 3 ]
-arr.should.have.property(0, 1);
-arr.should.have.property(1, 2);
-arr.should.have.property(2, 3);
-arr.should.have.lengthOf(3);
+it 'Should handle array literals' do
+  var arr = [ 1, 2, 3 ]
+  arr.should.have.property(0, 1)
+  arr.should.have.property(1, 2)
+  arr.should.have.property(2, 3)
+  arr.should.have.lengthOf(3)
+  (arr[0], arr[2]) = (arr[2], arr[0])
+  arr.should.have.property(0, 3)
+  arr.should.have.property(2, 1)
 
-(arr[0], arr[2]) = (arr[2], arr[0])
-arr.should.have.property(0, 3);
-arr.should.have.property(2, 1);
+it 'Should handle method calls' do
+  var obj = {
+    a: 1
+    b: 2
+  }
+  obj.m = () ->
+    this.a + this.b
+  obj.m().should.equal(3)
 
-obj.m = () ->
-  this.a + this.b
-obj.m().should.equal(3)
+it 'Should handle do blocks returning undefined' do
+  var obj = {
+    m1: ()->1
+    m2: ()->()
+    m3: ()->do
+      this.m1()
+      this.m2()
+  }
+  (typeof obj.m1()).should.equal('number')
+  (typeof obj.m2()).should.equal('undefined')
+  (typeof obj.m3()).should.equal('undefined')
+  obj.m1().should.equal 1
+  (obj.m2() == ()).should.equal(true)
+  (obj.m3() == ()).should.equal(true)
 
-var voidFunction = () -> do
-  ( (typeof voidFunction())
-    .should.equal('undefined')  )
+it 'Should handle string concatenation' do
+  var obj = {}
+  obj.world = ()->"world!"
+  ("Hello "+ obj.world()).should.equal('Hello world!')
+  obj = {
+    world: ()->"world!"
+  }
+  ("Hello "+ obj.world()).should.equal('Hello world!')
 
-obj.world = ()->"world!"
-("Hello "+ obj.world()).should.equal('Hello world!')
-
-obj = {
-  world: ()->"world!"
-}
-("Hello "+ obj.world()).should.equal('Hello world!')
-
-(() -> do
+it 'Should handle constructors' do
   var C = (a) -> do
     this.if = a
   var c = new C (4)
-  c.should.have.property('if', 4);
-)()
+  c.should.have.property('if', 4)
 
-meta
-  macro "moo"
-    predecence: KEY
-    expand: do
-      var code = \<- ('moo ' + (arg))
-      code.replaceTag('arg', expr.argAt(0))
-      give code
+it 'Should handle a simple macro' do
+  meta
+    macro "moo"
+      predecence: KEY
+      expand: do
+        var code = \<- ('moo ' + (arg))
+        code.replaceTag('arg', expr.argAt(0))
+        give code
+  (moo 42).should.equal('moo 42')
+  (moo 69).should.equal('moo 69')
+  (moo ('Hello '+ 'meta!')).should.equal('moo Hello meta!')
 
-(moo 42).should.equal('moo 42')
-(moo 69).should.equal('moo 69')
-(moo ('Hello '+ 'meta!')).should.equal('moo Hello meta!')
+it 'Should handle a macro involving \"this\"' do
+  meta
+    macro "@@@"
+      predecence: KEY
+      expand: do
+        var code = \<- this.arg
+        code.replaceTag('arg', expr.argAt(0))
+        give code
+  var obj = {
+    a: 1
+    b: 2
+    m: () -> (@@@a + @@@b)
+  }
+  (obj.m()).should.equal(3)
 
-meta
-  macro "@@@"
-    predecence: KEY
-    expand: do
-      var code = \<- this.arg
-      code.replaceTag('arg', expr.argAt(0))
-      give code
+it 'Should have a proper \"@\" operator' do
+  meta
+    macro '@'
+      predecence: KEY
+      expand: do
+        var member = expr.argAt(0)
+        var code = if (member.isTag())
+          \<- this.member
+          \<- this[member]
+        code.replaceTag('member', member)
+        give code
+  var obj = {
+    a: 1
+    b: 2
+  }
+  obj['aaa'] = 42
+  obj.m1 = () -> (@a + @b)
+  obj.m2 = (x, y) -> @(x + y)
+  (obj.m1()).should.equal(3)
+  (obj.m2('a', 'aa')).should.equal(42)
 
-obj = {
-  a: 1
-  b: 2
-  m: () -> (@@@a + @@@b)
-}
-(obj.m()).should.equal(3)
+it 'Should have macros that rename variables' do
+  meta
+    macro 'vTagTest'
+      predecence: KEY
+      expand: do
+        var count = expr.argAt(0)
+        var code = \<- do
+          var \result = []
+          loop (var \i = 0) do
+            if (\i < count) do
+              \result.push(\i)
+              next (\i + 1)
+            else end
+          give \result
+        code.replaceTag('count', count)
+        give code
+  var vTagTestN = vTagTest 3
+  vTagTestN.should.have.length(3)
+  vTagTestN.should.have.property(0, 0)
+  vTagTestN.should.have.property(1, 1)
+  vTagTestN.should.have.property(2, 2)
+  vTagTestN = vTagTest 2
+  vTagTestN.should.have.length(2)
+  vTagTestN.should.have.property(0, 0)
+  vTagTestN.should.have.property(1, 1)
 
-meta
-  macro '@'
-    predecence: KEY
-    expand: do
-      var member = expr.argAt(0)
-      var code = if (member.isTag())
-        \<- this.member
-        \<- this[member]
-      code.replaceTag('member', member)
-      give code
-obj['aaa'] = 42
-obj.m1 = () -> (@a + @b)
-obj.m2 = (x, y) -> @(x + y)
 
-(obj.m1()).should.equal(3)
-(obj.m2('a', 'aa')).should.equal(42)
+it 'Should handle the void operator' do
+  void (var a = 1)
+  a.should.equal 1
 
-require 'should'
-var util = require 'util'
 
-meta
-  macro 'vTagTest'
-    predecence: KEY
-    expand: do
-      var count = expr.argAt(0)
-      var code = \<- do
-        var \result = []
-        loop (var \i = 0) do
-          if (\i < count) do
-            \result.push(\i)
-            next (\i + 1)
-          else end
-        give \result
-      code.replaceTag('count', count)
-      give code
-
-var vTagTestN = vTagTest 3
-vTagTestN.should.have.length(3)
-vTagTestN.should.have.property(0, 0);
-vTagTestN.should.have.property(1, 1);
-vTagTestN.should.have.property(2, 2);
-vTagTestN = vTagTest 2
-vTagTestN.should.have.length(2)
-vTagTestN.should.have.property(0, 0);
-vTagTestN.should.have.property(1, 1);
-
+)
