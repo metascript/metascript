3dbbbd8c1b1d2c732c4ef30bbc5df96b77359165
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2014-06-16 22:02:38 +0200

    Renamed js macro as #js to avoid potential name claches.

--- test/functional/js-macro.mjs
+++ test/functional/js-macro.mjs
@@ -4,7 +4,7 @@ eval:40
 '''
 
 var foo = 10
-console.log( 'verbatim:' + js'foo + 20' )
+console.log( 'verbatim:' + #js'foo + 20' )
 
 var code = 'foo + 30'
-console.log( 'eval:' + js code)
+console.log( 'eval:' + #js code)
