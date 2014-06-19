fb8014becba59977af597c2d19dcbea92aaf5499
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2014-06-19 11:39:13 +0200

    Allow disabling scope for individual expressions.

---
+++ test/functional/scope-disabled.mjs
@@ -0,0 +1,18 @@
+'''
+42
+42
+42
+42
+'''
+
+do
+  #no-new-scope do
+    var a = 42
+    console.log a
+  console.log a
+
+do!
+  #no-new-scope do!
+    var a = 42
+    console.log a
+  console.log a
