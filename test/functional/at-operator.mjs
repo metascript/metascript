6e627758ebd454c896ebca0103981bdf9427968a
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2014-08-07 15:38:07 +0200

    Added tests to at operator, and one more way to reference this.

--- test/functional/at-operator.mjs
+++ test/functional/at-operator.mjs
@@ -3,6 +3,8 @@
 true
 true
 true
+true
+true
 '''
 
 var o = {
@@ -13,9 +15,13 @@ var o = {
   m2: #-> @[42]
   m3: #-> @[40 + 2]
   m4: #-> @42
+  m5: #-> @[]
+  m6: #-> (@)
 }
 
 console.log(o.m1())
 console.log(o.m2())
 console.log(o.m3())
 console.log(o.m4())
+console.log(o.m5() == o)
+console.log(o.m6() == o)
