1afbde50993dd8843c1a4c15519fbc72f004158e
Massimiliano Mantione <massimiliano.mantione@gmail.com>
2014-08-08 08:25:46 +0200

    Changed maybe operators, made ?? binary.

--- test/functional/maybe-dot-operator.mjs
+++ test/functional/maybe-dot-operator.mjs
@@ -1,15 +1,9 @@
 '''
 true
 true
-true
-false
-false
 false
 true
 true
-true
-false
-false
 false
 42
 true
@@ -18,6 +12,9 @@ true
 true
 true
 true
+true
+42
+42
 true
 '''
 
@@ -31,18 +28,12 @@ var o = {
 }
 
 console.log(o.p42 ?)
-console.log(o.p42 ??)
 console.log(o.p-null ?)
-console.log(o.p-null ??)
 console.log(o.k ?)
-console.log(o.k ??)
 
 console.log(o.o.p42 ?)
-console.log(o.o.p42 ??)
 console.log(o.o.p-null ?)
-console.log(o.o.p-null ??)
 console.log(o.o.k ?)
-console.log(o.o.k ??)
 
 console.log(o.?p42)
 console.log(o.?k == undefined)
@@ -54,3 +45,7 @@ console.log(o.?k.?k == undefined)
 console.log(o.?p-null.?p42 == null)
 console.log(o.?p-null.?p-null == null)
 console.log(o.?p-null.?k == null)
+
+console.log(o.p42 ?? false)
+console.log(o.k ?? 42)
+console.log((o.p-null ?? 42) == null)
